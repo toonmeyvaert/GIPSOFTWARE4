57d84ba2897810fe922ed8f81066de3f918d636f
Toon Meyvaert <toon.meyvaert@gmail.com>
2019-02-22 14:51:42 +0100

    dickpick

---
+++ Versie3/KlusjeK.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Versie3
+{
+    class KlusjeK
+    {
+        private int intSoort;
+        private int intVerdeeling;
+        private int intPlanning;
+
+        public int soort
+        {
+            get { return intSoort; }
+            set { value = intSoort; }
+        }
+        public int verdeeling
+        {
+            get { return intVerdeeling; }
+            set { value = intVerdeeling; }
+        }
+        public int planning
+        {
+            get { return intPlanning; }
+            set { value = intPlanning; }
+        }
+
+        public KlusjeK(int pintSoort, int pintVerdeeling, int pintPlanning)
+        {
+            intSoort = pintSoort;
+            intVerdeeling = pintVerdeeling;
+            intPlanning = pintPlanning;
+        }
+    }
+}
